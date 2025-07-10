82c47bbe1bd296106f22e70ada2353ef80dd03f1
Andrés López <andreslm3105@gmail.com>
2025-07-10 10:27:24 -0500

    nuevo boton adjuntar archivos v2

--- server.js
+++ server.js
@@ -36,6 +36,3 @@ pool.query('SELECT NOW()', (err, res) => {
 // Puerto
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Servidor backend corriendo en puerto ${PORT}`));
-
-
-
